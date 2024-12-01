6c86d6879256f5e5b162d3f37750769e57e5f0ce
Tharun74 <dev.tharun2005@gmail.com>
2024-12-01 14:56:53 +0530

    first commit

---
+++ components/AppBar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export default function AppBar(){
+    const session = useSession();
+
+    return <section className="border-b py-4">
+        <div className="container">
+           <div className="flex justify-between items-center">
+            <Link href={'/'} className="font-bold text-lg text-purple-500">Muzer</Link>
+            {session.data?.user && <Button onClick={() => signOut()}>Logout</Button>}
+            {!session.data?.user && <Button onClick={() => signIn()}>Get Started</Button>}
+        </div> 
+        </div>
+    </section>
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	container: {
+  		center: 'true',
+  		padding: {
+  			DEFAULT: '1rem',
+  			sm: '2rem',
+  			lg: '4rem',
+  			xl: '5rem',
+  			'2xl': '6rem'
+  		}
+  	},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
